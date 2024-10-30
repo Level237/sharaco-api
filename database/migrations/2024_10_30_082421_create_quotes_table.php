a791671fb8e1ee2b7a0402c40ac2026f1aeabb20
level237 <Bramslevel129@gmail.com>
2024-10-30 10:14:11 +0100

    update

---
+++ database/migrations/2024_10_30_082421_create_quotes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->id();
+            $table->string('quote_title');
+            $table->foreignIdFor(Model::class)
+            ->constrained()
+            ->restrictOnDelete()
+            ->restrictOnUpdate();
+            $table->string('quote_identified')->unique();
+            $table->string('client_name');
+            $table->string('client_logo');
+            $table->boolean('isTax')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quotes');
+    }
+};
