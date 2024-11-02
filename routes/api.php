db14aa25e2e53dd077eaeb0c9a7c1181541bfa50
level237 <Bramslevel129@gmail.com>
2024-11-02 19:01:42 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\Admin\UserController;
 use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\LogoutController;
 use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\User\ClientController;
 use App\Http\Controllers\Api\User\ProfileController;
 use App\Http\Controllers\Api\User\QuoteController;
 use Illuminate\Http\Request;
@@ -34,6 +35,7 @@ Route::middleware(['auth:api','scopes:admin'])->prefix('v1')->group(function(){
 
 Route::middleware(['auth:api'])->prefix('v1')->group(function(){
 
+    Route::apiResource('/clients',ClientController::class);
     Route::apiResource('/quotes',QuoteController::class);
     Route::get('/user/me',[ProfileController::class,'currentUser']);
     Route::post('/logout',[LogoutController::class,'logout']);
